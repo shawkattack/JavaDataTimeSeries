978114a66f0ea2754ab5f29057d442fe4cdbfee3
IIDUB <hrwegner@g.coastal.edu>
2015-12-06 19:53:35 -0500

    Basic Rule functionality

--- src/main/java/org/jdata/timeseries/processing/Rule.java
+++ src/main/java/org/jdata/timeseries/processing/Rule.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.jdata.timeseries.processing;
 
 /**
@@ -9,30 +6,100 @@ package org.jdata.timeseries.processing;
  */
 public class Rule {
 
-	int id;
-	int refCount;
-	Symbol head;
+    private int id;
+    private int refCount;
+    private Symbol head;
 
-	public Rule(int aId) {
+    public Rule(int aId) {
 
-		this.id = aId;
-		refCount = 0;
-		head = new Symbol(0);
-		head.setNext(head);
-		head.setPrev(head);
+        this.id = aId;
+        refCount = 0;
+        head = new Symbol(0);
+        head.setNext(head);                 
+        head.setPrev(head);
 
-	}
+    }
 
-	public int getRefCount() {
-		return refCount;
-	}
+    public void addReference() {
 
-	public void setRefCount(int refCount) {
-		this.refCount = refCount;
-	}
+        this.refCount = refCount + 1;
 
-	public int getId() {
-		return id;
-	}
+    }
+
+    public void removeReference() {
+
+        this.refCount = refCount - 1;
+
+    }
+
+    public void append(Symbol last) {
+
+        last = head.getPrev();
+
+    }
+
+    public void reduce(Symbol firstInPair, Rule replacement) {
+
+        Symbol sip = firstInPair.getNext();
+
+        Symbol prev = firstInPair.getPrev();
+
+        Symbol next = sip.getNext();
+
+        Symbol newSymbol = new Symbol(replacement.getId());
+
+        prev.setNext(newSymbol);
+
+        next.setPrev(newSymbol);
+
+        newSymbol.setNext(next);
+
+        newSymbol.setPrev(prev);
+
+        firstInPair.setNext(null);
+
+        firstInPair.setPrev(null);
+
+        sip.setNext(null);
+
+        sip.setPrev(null);
+
+    }
+
+    public void expand(Symbol ruleSymbol, Rule replacement) {
+
+        Symbol prev = ruleSymbol.getPrev();
+
+        Symbol next = ruleSymbol.getNext();
+
+        Symbol left = replacement.head.getNext();
+
+        Symbol right = replacement.head.getPrev();
+
+        prev.setNext(left);
+
+        left.setNext(prev);
+
+        next.setPrev(right);
+
+        right.setPrev(next);
+
+        ruleSymbol.setNext(null);
+
+        ruleSymbol.setPrev(null);
+
+        replacement.head.setNext(null);
+
+        replacement.head.setPrev(null);
+
+    }
+
+    public int getRefCount() {
+        return refCount;
+    }
+
+    public int getId() {
+        return id;
+    }
 
 }
